e2b94de523babab733681ddb4cbc731afab33a79
Shishir Suman <shishir.suman@tarento.com>
2025-07-22 16:46:54 +0530

    changes for pipleline

--- content-activity-updater/src/main/scala/org/sunbird/job/contentActivity/domain/Event.scala
+++ content-activity-updater/src/main/scala/org/sunbird/job/contentActivity/domain/Event.scala
@@ -4,26 +4,98 @@ import org.sunbird.job.domain.reader.JobRequest
 
 class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long)  extends JobRequest(eventMap, partition, offset) {
     
+    /**
+      * contains the event ID of the event
+      *
+      * @return
+      */
     def eventId: String = readOrDefault[String]("mid", "")
 
+    /**
+      * contain operation type of the event
+      * operation type can be CREATE or UPDATE
+      * This is used to identify the type of event
+      * and to process the event accordingly.
+      * @return
+      */ 
     def operationType: String = readOrDefault[String]("operationType", "")
 
+    /**
+      * contains do_id of a content
+      * @return
+      */
     def contentId: String = readOrDefault[String]("nodeUniqueId", "")
 
+    /**
+      * contains the created on date of the content
+      * @return
+      */
     def createdOn: String = readOrDefault[String]("transactionData.createdOn.nv", "")
 
+    /**
+      * contains the update date on date of the content
+      * @return
+      */
     def lastUpdatedOn: String = readOrDefault[String]("transactionData.lastUpdatedOn.nv", "")
 
+    /**
+      * contains the transaction data of the event
+      * transaction data contains the details of the content
+      * like created on date, last updated on date, status, channel, etc.
+      * transaction data is a map of key value pairs
+      * where key is the field name and value is the field value
+      * @return
+      */
     def transactionData: Map[String, AnyRef] = readOrDefault[Map[String, AnyRef]]("transactionData", Map[String, AnyRef]())
 
+    /**
+      * contains the last status changed on date of the content
+      *
+      * @return
+      */
     def lastStatusChangedOn: String = readOrDefault[String]("transactionData.lastStatusChangedOn.nv", "")
 
+    /**
+      * contains the channel or org ID of the content
+      *
+      * @return
+      */
     def channel: String = readOrDefault[String]("transactionData.channel.nv", "")
 
-    def status: String = readOrDefault[String]("transactionData.createdBy.nv", "")
+    /**
+      * contains status information of the content
+      *
+      * @return
+      */
+    def status: String = readOrDefault[String]("transactionData.status.nv", "")
 
+    /**
+      * contains creator information of the content
+      *
+      * @return
+      */
     def userId: String = readOrDefault[String]("userId", "")
 
+    /**
+      * contains creator information of the content
+      *
+      * @return
+      */
     def createdBy: String = readOrDefault[String]("transactionData.createdBy.nv", "")
 
+    def toMap: Map[String, Any] = {
+        Map(
+            "eventId" -> eventId,
+            "operationType" -> operationType,
+            "contentId" -> contentId,
+            "createdOn" -> createdOn,
+            "lastUpdatedOn" -> lastUpdatedOn,
+            "transactionData" -> transactionData,
+            "lastStatusChangedOn" -> lastStatusChangedOn,
+            "channel" -> channel,
+            "status" -> status,
+            "userId" -> userId,
+            "createdBy" -> createdBy
+        )
+    }
 }
--- content-activity-updater/src/main/scala/org/sunbird/job/contentActivity/functions/ContentActivityUpdaterFn.scala
+++ content-activity-updater/src/main/scala/org/sunbird/job/contentActivity/functions/ContentActivityUpdaterFn.scala
@@ -81,10 +81,42 @@ class ContentActivityUpdaterFn(config: ContentActivityUpdaterConfig, httpUtil: H
             metrics.incCounter(config.totalEventsCount)
             val userId = event.userId
             val operationType = event.operationType
-            val mid = event.mid
+            val eventId = event.eventId
             val contentId = event.contentId
             val status = event.status
-            logger.info(s"Event details - UserId: $userId, operationType: $operationType, mid: $mid, contentId: $contentId, Status: $status")       
+            val transactionData = event.transactionData
+            val createdOn = event.createdOn
+            val lastUpdatedOn = event.lastUpdatedOn
+            val lastStatusChangedOn = event.lastStatusChangedOn
+            val channel = event.channel
+            val createdBy = event.createdBy
+            // Validate the event
+            if (eventId.isEmpty || contentId.isEmpty || operationType.isBlank || transactionData.isEmpty) {
+                logger.error(s"Invalid event: ${event.eventId} for user: ${userId}")
+                throw new InvalidEventException(s"Invalid event: ${event.eventId} for user: ${userId}")
+            }
+            logger.info(s"Event details - UserId: $userId, operationType: $operationType, mid: $eventId, contentId: $contentId, Status: $status")
+            // Check if the event is already processed
+            val eventExists = checkEventExists(eventId)
+            if (eventExists) {
+                logger.info(s"Event already processed: $eventId for user: $userId")
+                metrics.incCounter(config.skippedEventCount)
+                // update existing event in the database
+                val updateQuery = s"UPDATE content_activity SET status = ?, last_updated_on = ?, last_status_changed_on = ?, channel = ?, transaction_data = ? WHERE event_id = ?"
+                executeInsert(updateQuery, List(status, lastUpdatedOn, lastStatusChangedOn, channel, mapper.writeValueAsString(transactionData), eventId))
+                metrics.incCounter(config.dbUpdateCount)
+                metrics.incCounter(config.successEventCount)
+                return
+                } else {
+                logger.info(s"Processing new event: $eventId for user: $userId")
+                metrics.incCounter(config.dbReadCount)
+                // Insert new event into the database
+                val insertQuery = s"INSERT INTO content_activity (event_id, user_id, content_id, operation_type, status, created_on, last_updated_on, last_status_changed_on, channel, created_by, transaction_data) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                executeInsert(insertQuery, List(eventId, userId, contentId, operationType, status, createdOn, lastUpdatedOn, lastStatusChangedOn, channel, createdBy, mapper.writeValueAsString(transactionData)))
+                metrics.incCounter(config.dbUpdateCount)
+                metrics.incCounter(config.successEventCount)
+                logger.info(s"Event processed successfully: $eventId for user: $userId")      
+                }
             } catch {
             case e: Exception =>
                 logger.error(s"Error processing event: ${event.eventId} for user: ${event.userId}", e)
@@ -93,13 +125,75 @@ class ContentActivityUpdaterFn(config: ContentActivityUpdaterConfig, httpUtil: H
             }
         }
     
+    def checkEventExists(eventId: String): Boolean ={
+        var connection: Option[Connection] = None
+        var preparedStatement: Option[PreparedStatement] = None
+        var resultSet: Option[ResultSet] = None
+        try {
+            val connectionUrl = s"jdbc:postgresql://${config.postgresDbHost}:${config.postgresDbPort}/${config.postgresDbDatabase}"
+            connection = Some(DriverManager.getConnection(connectionUrl, config.postgresDbUsername, config.postgresDbPassword))
+            preparedStatement = Some(connection.get.prepareStatement("SELECT COUNT(*) FROM content_activity WHERE event_id = ?"))
+            preparedStatement.get.setString(1, eventId)
+            resultSet = Some(preparedStatement.get.executeQuery())
+            if (resultSet.get.next()) {
+                resultSet.get.getInt(1) > 0
+            } else {
+                false
+            }
+        } catch {
+            case ex: Exception =>
+                logger.error(s"Error checking event existence for eventId: $eventId", ex)
+                false
+        } finally {
+            resultSet.foreach(_.close())
+            preparedStatement.foreach(_.close())
+            connection.foreach(_.close())
+        }
+    }
+
+    /**
+      * method to execute a query on the PostgreSQL database
+      * @param query
+      */
+    def executeQuery(query: String): List[Event] = {
+        var connection: Option[Connection] = None
+        var preparedStatement: Option[PreparedStatement] = None
+        var resultSet: Option[ResultSet] = None
+        var events: List[Event] = List()
+
+        try {
+            val connectionUrl = s"jdbc:postgresql://${config.postgresDbHost}:${config.postgresDbPort}/${config.postgresDbDatabase}"
+            val connection = DriverManager.getConnection(connectionUrl, config.postgresDbUsername, config.postgresDbPassword)
+            connection.setAutoCommit(true)
+            preparedStatement = Some(connection.prepareStatement(query))
+            logger.info(s"query statement created. $preparedStatement")
+            resultSet = Some(preparedStatement.get.executeQuery())
+            // TODO extract resultset to Event objects
+            if (events.isEmpty) {
+                logger.info("No events found for the given query.")
+                return List()
+            }
+            logger.info(s"Query executed successfully. ${events.size} events fetched.")
+            return events
+        } catch {
+            case ex: Exception =>
+                println(s"Error during insert: ${ex.getMessage}")
+                return List()
+        } finally {
+            resultSet.foreach(_.close())
+            preparedStatement.foreach(_.close())
+            connection.foreach(_.close())
+        }
+    }
+    
     /**
       * method to execute an update query on the PostgreSQL database
       * @param query
       */
     def executeUpdate(query: String): Unit = {
-        var connectionInsert: Option[Connection] = None
+        var connection: Option[Connection] = None
         var preparedStatement: Option[PreparedStatement] = None
+        var resultSet: Option[ResultSet] = None
         try {
             val connectionUrl = s"jdbc:postgresql://${config.postgresDbHost}:${config.postgresDbPort}/${config.postgresDbDatabase}"
             val connection = DriverManager.getConnection(connectionUrl, config.postgresDbUsername, config.postgresDbPassword)
@@ -112,7 +206,9 @@ class ContentActivityUpdaterFn(config: ContentActivityUpdaterConfig, httpUtil: H
             case ex: Exception =>
                 println(s"Error during insert: ${ex.getMessage}")
         } finally {
+            resultSet.foreach(_.close())
             preparedStatement.foreach(_.close())
+            connection.foreach(_.close())
         }
     }
 
@@ -123,8 +219,9 @@ class ContentActivityUpdaterFn(config: ContentActivityUpdaterConfig, httpUtil: H
      * @param params The parameters to be set in the prepared statement.
      */
     def executeInsert(query: String, params: Seq[Any]): Unit = {
-        var connectionInsert: Option[Connection] = None
+        var connection: Option[Connection] = None
         var preparedStatement: Option[PreparedStatement] = None
+        var resultSet: Option[ResultSet] = None
         try {
             val connectionUrl = s"jdbc:postgresql://${config.postgresDbHost}:${config.postgresDbPort}/${config.postgresDbDatabase}"
             val connection = DriverManager.getConnection(connectionUrl, config.postgresDbUsername, config.postgresDbPassword)
@@ -140,7 +237,9 @@ class ContentActivityUpdaterFn(config: ContentActivityUpdaterConfig, httpUtil: H
             case ex: Exception =>
                 println(s"Error during insert: ${ex.getMessage}")
         } finally {
+           resultSet.foreach(_.close())
             preparedStatement.foreach(_.close())
+            connection.foreach(_.close())
         }
     }
 }
